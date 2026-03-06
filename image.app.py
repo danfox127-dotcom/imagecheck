69cb4a0de406d9666db9450c5810ea91dfa05b19
danfox127-dotcom <danfox127@gmail.com>
2026-03-06 09:46:06 -0500

    Update image.app.py

--- image.app.py
+++ image.app.py
@@ -8,7 +8,7 @@ import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # --- Configuration ---
-CSV_FILE = "columbiadoctors_images.csv"
+CSV_FILE = "columbiadoctors_images_used_3_6.csv"
 
 # --- Sidebar Debug ---
 st.sidebar.subheader("System Debugger")
